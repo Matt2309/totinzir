5cebab13d222cee5d67770d29f011f4816b684a9
Mattia Mularoni <mattia.mularoni@studio.unibo.it>
2025-06-06 22:41:30 +0200

    checkout redirect if not logged in

--- src/app/checkout/layout.tsx
+++ src/app/checkout/layout.tsx
@@ -1,7 +1,15 @@
 // app/special/layout.tsx
 import '@/app/globals.css';
+import {verifySession} from "@/lib/dal";
+import {redirect} from "next/navigation";
 
-export default function SpecialLayout({ children }: { children: React.ReactNode }) {
+export default async function SpecialLayout({children}: { children: React.ReactNode }) {
+    const session = await verifySession();
+    const user = session?.userId;
+    if (!user) {
+        redirect('/login')
+        return;
+    }
     return (
         <div className="bg-detail min-h-screen">
             {children}
